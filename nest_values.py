8dda46b06af16348a8509283c44ed7dfb1e0d3c0
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-04 17:56:17 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -6,7 +6,7 @@ results_path = 'results_folder'
 positions_path = 'positions_folder'
 input_images_path = 'input_images_folder' 
 gabor_folder = 'gabor_outputs'
-collect_data_folder = 'results_collect_data_big_edge'
+collect_data_folder = 'results_collect_data_small_edge'
 
 
 #Simulation 
