4c9d670df7d8737de22df4670903a4361a2f0895
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-14 17:40:19 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -99,13 +99,13 @@ rescale = 2.0 ; radius_lat = 2.5
 
 # Short lateral connections
 ratio_inh_exc_w = 4.0
-p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.3
+p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.2
 p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.1
 p_center_exc_exc = 1.0 ;        weight_exc_exc =  0.6
 p_center_exc_inh = 1.0 ;        weight_exc_inh =  0.8
 
 input_weight_poiss_exc = 1.0     
-input_weight_poiss_inh = 0.15
+input_weight_poiss_inh = 0.3
                
 dict_poiss_to_v1_exc  = {'connection_type': 'divergent','weights': input_weight_poiss_exc ,
                          'mask': {'grid': {'rows': 1, 'columns': 1}}, 
