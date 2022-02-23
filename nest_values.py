3dfe08bdbddd567fcf84adee345b75ced2035dfc
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-23 16:18:55 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -6,7 +6,7 @@ results_path = 'results_folder'
 positions_path = 'positions_folder'
 input_images_path = 'input_images_folder' 
 gabor_folder = 'gabor_outputs'
-collect_data_folder = 'results_collect_data_56'
+collect_data_folder = 'results_collect_data_58'
 
 
 #Simulation 
@@ -86,23 +86,23 @@ syn_model_inh = 'static_synapse_hpc'
 syn_model_exc = 'static_synapse_hpc' 
 
 #Delays
-delay_exc = 0.3 ; delay_exc_large = 1.0 ; delay_inh = 0.5
+delay_exc = 0.25 ; delay_exc_large = 1.0 ; delay_inh = 0.5
 slowness_exc = 0.5; slowness_exc_large = 0.5; slowness_inh = 0.5
 
 
 # Large lateral connections
-kappa_j = 0.126 * 1.5
-kappa_w = 0.14 * 2.5
-weight_large_range_exc_exc = 0.006
+kappa_j = 0.126 * 1
+kappa_w = 0.14 * 1
+weight_large_range_exc_exc = 0.005
 weight_large_range_exc_inh = 0.02
 rescale = 1.0 ; radius_lat = 3.5#2.5
 
 # Short lateral connections
 ratio_inh_exc_w = 4.0
 p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.2
-p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.175
+p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.15
 p_center_exc_exc = 1.0 ;        weight_exc_exc =  0.5
-p_center_exc_inh = 1.0 ;        weight_exc_inh =  0.6
+p_center_exc_inh = 1.0 ;        weight_exc_inh =  0.3
 
 input_weight_poiss_exc = 1.0     
 input_weight_poiss_inh = 0.25
