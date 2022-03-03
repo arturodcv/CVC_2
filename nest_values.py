050b8d02b4fed33a1984cc88cb275002535338f7
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-03-03 12:00:43 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -42,11 +42,11 @@ extent = [float(num_hipercolumns), float(num_hipercolumns)]
 ratio_exc_inh = 4
 neurons_per_column_inh = 5
 neurons_per_column_exc = ratio_exc_inh * neurons_per_column_inh 
-poisson_bias = 0.0
+poisson_bias = 4.0
 
 #Poisson
 factor = 430.9
-factor_bias = 0.0
+factor_bias = 430.9
 
 ############ Dictionaries
 
